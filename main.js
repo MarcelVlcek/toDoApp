2b40a57b6c522ee798f5230831481e423bd4059d
Marcel Vlcek <vlcek.marc@gmail.com>
2024-11-07 17:19:50 +0100

    localStorage

--- main.js
+++ main.js
@@ -1,22 +1,40 @@
 
 
-function createNewLi() {
+if(localStorage.getItem('tasks') == null){
+    var taskArray = [];
+}else{
+    taskArray = JSON.parse(localStorage.getItem('tasks'))
+}
+
     let myForm = document.querySelector('#task-form');
     myForm.addEventListener('submit', function(event) {
         
         event.preventDefault();
 
-        let newLi = document.createElement('li');
-        newLi.textContent = event.target.task.value;
+        taskArray.push(event.target.elements.task.value);
+        taskArrayToLS = JSON.stringify(taskArray);
+        localStorage.setItem('tasks', taskArrayToLS);
 
-        newLi.classList.add('li-style')
+        event.target.elements.task.value = "";
 
-        let taskList = document.querySelector('.tasks');
-        taskList.appendChild(newLi);
+        let taskArrayFromLS = localStorage.getItem('tasks');
+        let taskArrayFromLSj = JSON.parse(taskArrayFromLS);
 
-        event.target.task.value = ''
+        let paragraph = document.createElement('p')
+        paragraph.textContent = taskArrayFromLSj[taskArrayFromLSj.length - 1];
+        document.querySelector('.tasks').appendChild(paragraph);
+    
+    
     })
 
+let presentTaskArray = localStorage.getItem('tasks');
+let presentTaskArrayj = JSON.parse(presentTaskArray);
 
+
+if(presentTaskArrayj !== null){
+    presentTaskArrayj.forEach(function(oneTask){
+        let paragraph = document.createElement('p');
+        paragraph.textContent = oneTask;
+        document.querySelector('.tasks').appendChild(paragraph);
+    })  
 }
-createNewLi();
